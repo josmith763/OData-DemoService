db78fd7631ab9e300a4d5d0f96d6a0e212329f3c
Joshua Smith <jsmith20159763@outlook.com>
2021-03-31 16:27:17 -0400

    Started skeleton

--- src/main/java/myservice/mynamespace/data/Storage.java
+++ src/main/java/myservice/mynamespace/data/Storage.java
@@ -46,32 +46,19 @@ public class Storage {
 
     // represent our database
     private List<Entity> productList;
-    private List<Entity> categoryList;
-    private List<Entity> supplierList;
 
     public Storage() {
 
         productList = new ArrayList<Entity>();
-        categoryList = new ArrayList<Entity>();
-        supplierList = new ArrayList<Entity>();
-
-        // creating some sample data
-        initProductSampleData();
-        initCategorySampleData();
-        initSupplierSampleData();
     }
 
     /* PUBLIC FACADE */
-
     public EntityCollection readEntitySetData(EdmEntitySet edmEntitySet) {
         EntityCollection entitySet = null;
 
         if (edmEntitySet.getName().equals(DemoEdmProvider.ES_PRODUCTS_NAME)) {
             entitySet = getProducts();
-        } else if (edmEntitySet.getName().equals(DemoEdmProvider.ES_CATEGORIES_NAME)) {
-            entitySet = getCategories();
         }
-
         return entitySet;
     }
 
@@ -82,12 +69,7 @@ public class Storage {
 
         if (edmEntityType.getName().equals(DemoEdmProvider.ET_PRODUCT_NAME)) {
             entity = getProduct(edmEntityType, keyParams);
-        } else if (edmEntityType.getName().equals(DemoEdmProvider.ET_CATEGORY_NAME)) {
-            entity = getCategory(edmEntityType, keyParams);
-        } else if (edmEntityType.getName().equals(DemoEdmProvider.ET_SUPPLIER_NAME)) {
-            entity = getSupplier(edmEntityType, keyParams);
         }
-
         return entity;
     }
 
@@ -144,10 +126,10 @@ public class Storage {
         return Util.findEntity(relatedEntityType, relatedEntities, keyPredicates);
     }
 
-    public EntityCollection getRelatedEntityCollection(Entity sourceEntity, EdmEntityType targetEntityType) {
+   public EntityCollection getRelatedEntityCollection(Entity sourceEntity, EdmEntityType targetEntityType) {
         EntityCollection navigationTargetEntityCollection = new EntityCollection();
 
-        FullQualifiedName relatedEntityFqn = targetEntityType.getFullQualifiedName();
+      /*  FullQualifiedName relatedEntityFqn = targetEntityType.getFullQualifiedName();
         String sourceEntityFqn = sourceEntity.getType();
 
         if (sourceEntityFqn.equals(DemoEdmProvider.ET_PRODUCT_FQN.getFullQualifiedNameAsString())
@@ -196,7 +178,7 @@ public class Storage {
 
         if (navigationTargetEntityCollection.getEntities().isEmpty()) {
             return null;
-        }
+        }*/
 
         return navigationTargetEntityCollection;
     }
@@ -213,16 +195,6 @@ public class Storage {
         return retEntitySet;
     }
 
-    private EntityCollection getSuppliers() {
-        EntityCollection retEntitySet = new EntityCollection();
-
-        for (Entity supplierEntity : this.supplierList) {
-            retEntitySet.getEntities().add(supplierEntity);
-        }
-
-        return retEntitySet;
-    }
-
     private Entity getProduct(EdmEntityType edmEntityType, List<UriParameter> keyParams) throws ODataApplicationException {
 
         // the list of entities at runtime
@@ -230,34 +202,6 @@ public class Storage {
 
         /* generic approach to find the requested entity */
         return Util.findEntity(edmEntityType, entityCollection, keyParams);
-    }
-
-    private Entity getSupplier(EdmEntityType edmEntityType, List<UriParameter> keyParams) throws ODataApplicationException {
-
-        // the list of entities at runtime
-        EntityCollection entityCollection = getSuppliers();
-
-        /* generic approach to find the requested entity */
-        return Util.findEntity(edmEntityType, entityCollection, keyParams);
-    }
-
-    private EntityCollection getCategories() {
-        EntityCollection entitySet = new EntityCollection();
-
-        for (Entity categoryEntity : this.categoryList) {
-            entitySet.getEntities().add(categoryEntity);
-        }
-
-        return entitySet;
-    }
-
-    private Entity getCategory(EdmEntityType edmEntityType, List<UriParameter> keyParams) throws ODataApplicationException {
-
-        // the list of entities at runtime
-        EntityCollection entitySet = getCategories();
-
-        /* generic approach to find the requested entity */
-        return Util.findEntity(edmEntityType, entitySet, keyParams);
     }
 
     private Entity createProduct(EdmEntityType edmEntityType, Entity entity) {
@@ -357,206 +301,6 @@ public class Storage {
         return false;
     }
 
-    private void initProductSampleData() {
-
-        Entity entity = new Entity();
-
-        entity.addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 1));
-        entity.addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "Notebook Basic 15"));
-        entity.addProperty(new Property(null, "Description", ValueType.PRIMITIVE,
-                "Notebook Basic, 1.7GHz - 15 XGA - 1024MB DDR2 SDRAM - 40GB"));
-        entity.setType(DemoEdmProvider.ET_PRODUCT_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "ID"));
-        productList.add(entity);
-
-        entity = new Entity();
-        entity.addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 2));
-        entity.addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "Notebook Professional 17"));
-        entity.addProperty(new Property(null, "Description", ValueType.PRIMITIVE,
-                "Notebook Professional, 2.8GHz - 15 XGA - 8GB DDR3 RAM - 500GB"));
-        entity.setType(DemoEdmProvider.ET_PRODUCT_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "ID"));
-        productList.add(entity);
-
-        entity = new Entity();
-        entity.addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 3));
-        entity.addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "1UMTS PDA"));
-        entity.addProperty(new Property(null, "Description", ValueType.PRIMITIVE,
-                "Ultrafast 3G UMTS/HSDPA Pocket PC, supports GSM network"));
-        entity.setType(DemoEdmProvider.ET_PRODUCT_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "ID"));
-        productList.add(entity);
-
-        entity = new Entity();
-        entity.addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 4));
-        entity.addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "Comfort Easy"));
-        entity.addProperty(new Property(null, "Description", ValueType.PRIMITIVE,
-                "32 GB Digital Assitant with high-resolution color screen"));
-        entity.setType(DemoEdmProvider.ET_PRODUCT_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "ID"));
-        productList.add(entity);
-
-        entity = new Entity();
-        entity.addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 5));
-        entity.addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "Ergo Screen"));
-        entity.addProperty(new Property(null, "Description", ValueType.PRIMITIVE,
-                "19 Optimum Resolution 1024 x 768 @ 85Hz, resolution 1280 x 960"));
-        entity.setType(DemoEdmProvider.ET_PRODUCT_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "ID"));
-        productList.add(entity);
-
-        entity = new Entity();
-        entity.addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 6));
-        entity.addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "Flat Basic"));
-        entity.addProperty(new Property(null, "Description", ValueType.PRIMITIVE,
-                "Optimum Hi-Resolution max. 1600 x 1200 @ 85Hz, Dot Pitch: 0.24mm"));
-        entity.setType(DemoEdmProvider.ET_PRODUCT_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "ID"));
-        productList.add(entity);
-    }
-
-    private void initCategorySampleData() {
-
-        Entity entity = new Entity();
-
-        entity.addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 1));
-        entity.addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "Notebooks"));
-        entity.setType(DemoEdmProvider.ET_CATEGORY_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "ID"));
-        categoryList.add(entity);
-
-        entity = new Entity();
-        entity.addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 2));
-        entity.addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "Organizers"));
-        entity.setType(DemoEdmProvider.ET_CATEGORY_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "ID"));
-        categoryList.add(entity);
-
-        entity = new Entity();
-        entity.addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 3));
-        entity.addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "Monitors"));
-        entity.setType(DemoEdmProvider.ET_CATEGORY_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "ID"));
-        categoryList.add(entity);
-    }
-
-    private void initSupplierSampleData() {
-
-        Entity entity = new Entity();
-
-        entity.addProperty(new Property(null, "SupplierID", ValueType.PRIMITIVE, 1));
-        entity.addProperty(new Property(null, "CompanyName", ValueType.PRIMITIVE, "DESIGN INDEX INDIA"));
-        entity.addProperty(new Property(null, "Fax", ValueType.PRIMITIVE,
-                "null"));
-
-        Property p1 = new Property(null, "City", ValueType.PRIMITIVE, "Delhi");
-        Property p2 = new Property(null, "Country", ValueType.PRIMITIVE, "India");
-        ComplexValue complexValue = new ComplexValue();
-        complexValue.getValue().add(p1);
-        complexValue.getValue().add(p2);
-
-        entity.addProperty(new Property(null, "Address", ValueType.COMPLEX, complexValue));
-        entity.setType(DemoEdmProvider.ET_SUPPLIER_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "SupplierID", "Suppliers", "1"));
-        supplierList.add(entity);
-
-        entity = new Entity();
-        entity.addProperty(new Property(null, "SupplierID", ValueType.PRIMITIVE, 2));
-        entity.addProperty(new Property(null, "CompanyName", ValueType.PRIMITIVE, "Advance Label Ltd"));
-        entity.addProperty(new Property(null, "Fax", ValueType.PRIMITIVE,
-                "(617) 555-3389"));
-        p1 = new Property(null, "City", ValueType.PRIMITIVE, "Tsuen Wan");
-        p2 = new Property(null, "Country", ValueType.PRIMITIVE, "Hong Kong");
-        complexValue = new ComplexValue();
-        complexValue.getValue().add(p1);
-        complexValue.getValue().add(p2);
-
-        entity.addProperty(new Property(null, "Address", ValueType.COMPLEX, complexValue));
-        entity.setType(DemoEdmProvider.ET_SUPPLIER_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "SupplierID", "Suppliers", "1"));
-        supplierList.add(entity);
-
-        entity = new Entity();
-        entity.addProperty(new Property(null, "SupplierID", ValueType.PRIMITIVE, 3));
-        entity.addProperty(new Property(null, "CompanyName", ValueType.PRIMITIVE, "TECH GROUP INTERNATIONAL LIMITED "));
-        entity.addProperty(new Property(null, "Fax", ValueType.PRIMITIVE,
-                "null"));
-        p1 = new Property(null, "City", ValueType.PRIMITIVE, "Bangalore");
-        p2 = new Property(null, "Country", ValueType.PRIMITIVE, "India");
-        complexValue = new ComplexValue();
-        complexValue.getValue().add(p1);
-        complexValue.getValue().add(p2);
-
-        entity.addProperty(new Property(null, "Address", ValueType.COMPLEX, complexValue));
-        entity.setType(DemoEdmProvider.ET_SUPPLIER_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "SupplierID", "Suppliers", "2"));
-        supplierList.add(entity);
-
-        entity = new Entity();
-        entity.addProperty(new Property(null, "SupplierID", ValueType.PRIMITIVE, 4));
-        entity.addProperty(new Property(null, "CompanyName", ValueType.PRIMITIVE, "BenQ India Pvt Ltd"));
-        entity.addProperty(new Property(null, "Fax", ValueType.PRIMITIVE,
-                "43844115"));
-        p1 = new Property(null, "City", ValueType.PRIMITIVE, "Gurgaon");
-        p2 = new Property(null, "Country", ValueType.PRIMITIVE, "India");
-        complexValue = new ComplexValue();
-        complexValue.getValue().add(p1);
-        complexValue.getValue().add(p2);
-
-        entity.addProperty(new Property(null, "Address", ValueType.COMPLEX, complexValue));
-        entity.setType(DemoEdmProvider.ET_SUPPLIER_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "SupplierID", "Suppliers", "3"));
-        supplierList.add(entity);
-
-        entity = new Entity();
-        entity.addProperty(new Property(null, "SupplierID", ValueType.PRIMITIVE, 5));
-        entity.addProperty(new Property(null, "CompanyName", ValueType.PRIMITIVE, "Sharp"));
-        entity.addProperty(new Property(null, "Fax", ValueType.PRIMITIVE,
-                "(12345) 1210"));
-        p1 = new Property(null, "City", ValueType.PRIMITIVE, "Osaka");
-        p2 = new Property(null, "Country", ValueType.PRIMITIVE, "Japan");
-        complexValue = new ComplexValue();
-        complexValue.getValue().add(p1);
-        complexValue.getValue().add(p2);
-
-        entity.addProperty(new Property(null, "Address", ValueType.COMPLEX, complexValue));
-        entity.setType(DemoEdmProvider.ET_SUPPLIER_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "SupplierID", "Suppliers", "4"));
-        supplierList.add(entity);
-
-        entity = new Entity();
-        entity.addProperty(new Property(null, "SupplierID", ValueType.PRIMITIVE, 6));
-        entity.addProperty(new Property(null, "CompanyName", ValueType.PRIMITIVE, "Wonder World"));
-        entity.addProperty(new Property(null, "Fax", ValueType.PRIMITIVE,
-                "43844115"));
-        p1 = new Property(null, "City", ValueType.PRIMITIVE, "Zürich");
-        p2 = new Property(null, "Country", ValueType.PRIMITIVE, "Switzerland");
-        complexValue = new ComplexValue();
-        complexValue.getValue().add(p1);
-        complexValue.getValue().add(p2);
-
-        entity.addProperty(new Property(null, "Address", ValueType.COMPLEX, complexValue));
-        entity.setType(DemoEdmProvider.ET_SUPPLIER_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "SupplierID", "Suppliers", "5"));
-        supplierList.add(entity);
-
-        entity = new Entity();
-        entity.addProperty(new Property(null, "SupplierID", ValueType.PRIMITIVE, 7));
-        entity.addProperty(new Property(null, "CompanyName", ValueType.PRIMITIVE, "Touch International"));
-        entity.addProperty(new Property(null, "Fax", ValueType.PRIMITIVE,
-                "43847115"));
-        p1 = new Property(null, "City", ValueType.PRIMITIVE, "Austin");
-        p2 = new Property(null, "Country", ValueType.PRIMITIVE, "USA");
-        complexValue = new ComplexValue();
-        complexValue.getValue().add(p1);
-        complexValue.getValue().add(p2);
-
-        entity.addProperty(new Property(null, "Address", ValueType.COMPLEX, complexValue));
-        entity.setType(DemoEdmProvider.ET_SUPPLIER_FQN.getFullQualifiedNameAsString());
-        entity.setId(createId(entity, "SupplierID", "Suppliers", "6"));
-        supplierList.add(entity);
-    }
-
     private URI createId(Entity entity, String idPropertyName) {
         return createId(entity, idPropertyName, null);
     }
@@ -591,11 +335,7 @@ public class Storage {
     }
 
     private String getEntitySetName(Entity entity) {
-        if(DemoEdmProvider.ET_CATEGORY_FQN.getFullQualifiedNameAsString().equals(entity.getType())) {
-            return DemoEdmProvider.ES_CATEGORIES_NAME;
-        } else if(DemoEdmProvider.ET_PRODUCT_FQN.getFullQualifiedNameAsString().equals(entity.getType())) {
-            return DemoEdmProvider.ES_PRODUCTS_NAME;
-        } else if(DemoEdmProvider.ET_SUPPLIER_FQN.getFullQualifiedNameAsString().equals(entity.getType())) {
+        if(DemoEdmProvider.ET_PRODUCT_FQN.getFullQualifiedNameAsString().equals(entity.getType())) {
             return DemoEdmProvider.ES_PRODUCTS_NAME;
         }
         return entity.getType();
--- src/main/java/myservice/mynamespace/service/DemoEdmProvider.java
+++ src/main/java/myservice/mynamespace/service/DemoEdmProvider.java
@@ -49,19 +49,8 @@ public class DemoEdmProvider extends CsdlAbstractEdmProvider {
     public static final String ET_PRODUCT_NAME = "Product";
     public static final FullQualifiedName ET_PRODUCT_FQN = new FullQualifiedName(NAMESPACE, ET_PRODUCT_NAME);
 
-    public static final String ET_CATEGORY_NAME = "Category";
-    public static final FullQualifiedName ET_CATEGORY_FQN = new FullQualifiedName(NAMESPACE, ET_CATEGORY_NAME);
-
-    public static final String ET_SUPPLIER_NAME = "Supplier";
-    public static final FullQualifiedName ET_SUPPLIER_FQN = new FullQualifiedName(NAMESPACE, ET_SUPPLIER_NAME);
-
-    public static final String CT_ADDRESS_NAME = "Address";
-    public static final FullQualifiedName CT_ADDRESS_FQN = new FullQualifiedName(NAMESPACE, CT_ADDRESS_NAME);
-
     // Entity Set Names
     public static final String ES_PRODUCTS_NAME = "Products";
-    public static final String ES_CATEGORIES_NAME = "Categories";
-    public static final String ES_SUPPLIERS_NAME = "Suppliers";
 
     @Override
     public CsdlEntityType getEntityType(FullQualifiedName entityTypeName) {
@@ -71,96 +60,27 @@ public class DemoEdmProvider extends CsdlAbstractEdmProvider {
 
         if (entityTypeName.equals(ET_PRODUCT_FQN)) {
             // create EntityType properties
-            CsdlProperty id = new CsdlProperty().setName("ID")
+           /* CsdlProperty id = new CsdlProperty().setName("ID")
                     .setType(EdmPrimitiveTypeKind.Int32.getFullQualifiedName());
             CsdlProperty name = new CsdlProperty().setName("Name")
                     .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
             CsdlProperty description = new CsdlProperty().setName("Description")
-                    .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
+                    .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());*/
 
             // create PropertyRef for Key element
-            CsdlPropertyRef propertyRef = new CsdlPropertyRef();
-            propertyRef.setName("ID");
-
-            // navigation property: many-to-one, null not allowed (product must have a category)
-            CsdlNavigationProperty navProp = new CsdlNavigationProperty().setName("Category")
-                    .setType(ET_CATEGORY_FQN).setNullable(false).setPartner("Products");
-            CsdlNavigationProperty contNavProp = new CsdlNavigationProperty().setName("Suppliers")
-                    .setType(ET_SUPPLIER_FQN).setContainsTarget(true).setCollection(true);
-            List<CsdlNavigationProperty> navPropList = new ArrayList<CsdlNavigationProperty>();
-            navPropList.add(navProp);
-            navPropList.add(contNavProp);
+            //CsdlPropertyRef propertyRef = new CsdlPropertyRef();
+            //propertyRef.setName("ID");
 
             // configure EntityType
             entityType = new CsdlEntityType();
             entityType.setName(ET_PRODUCT_NAME);
-            entityType.setProperties(Arrays.asList(id, name, description));
-            entityType.setKey(Arrays.asList(propertyRef));
-            entityType.setNavigationProperties(navPropList);
+            //entityType.setProperties(Arrays.asList(id, name, description));
+            //entityType.setKey(Arrays.asList(propertyRef));
 
-        } else if (entityTypeName.equals(ET_CATEGORY_FQN)) {
-            // create EntityType properties
-            CsdlProperty id = new CsdlProperty().setName("ID")
-                    .setType(EdmPrimitiveTypeKind.Int32.getFullQualifiedName());
-            CsdlProperty name = new CsdlProperty().setName("Name")
-                    .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
-
-            // create PropertyRef for Key element
-            CsdlPropertyRef propertyRef = new CsdlPropertyRef();
-            propertyRef.setName("ID");
-
-            // navigation property: one-to-many
-            CsdlNavigationProperty navProp = new CsdlNavigationProperty().setName("Products")
-                    .setType(ET_PRODUCT_FQN).setCollection(true).setPartner("Category");
-            List<CsdlNavigationProperty> navPropList = new ArrayList<CsdlNavigationProperty>();
-            navPropList.add(navProp);
-
-            // configure EntityType
-            entityType = new CsdlEntityType();
-            entityType.setName(ET_CATEGORY_NAME);
-            entityType.setProperties(Arrays.asList(id, name));
-            entityType.setKey(Arrays.asList(propertyRef));
-            entityType.setNavigationProperties(navPropList);
-        } else if (entityTypeName.equals(ET_SUPPLIER_FQN)) {
-            // create EntityType properties
-            CsdlProperty supplierId = new CsdlProperty().setName("SupplierID")
-                    .setType(EdmPrimitiveTypeKind.Int32.getFullQualifiedName());
-            CsdlProperty companyName = new CsdlProperty().setName("CompanyName")
-                    .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
-            CsdlProperty fax = new CsdlProperty().setName("Fax")
-                    .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
-            CsdlProperty address = new CsdlProperty().setName(CT_ADDRESS_NAME)
-                    .setType(CT_ADDRESS_FQN);
-
-            // create PropertyRef for Key element
-            CsdlPropertyRef propertyRef = new CsdlPropertyRef();
-            propertyRef.setName("SupplierID");
-
-            // configure EntityType
-            entityType = new CsdlEntityType();
-            entityType.setName(ET_SUPPLIER_NAME);
-            entityType.setProperties(Arrays.asList(supplierId, companyName, fax, address));
-            entityType.setKey(Arrays.asList(propertyRef));
         }
 
         return entityType;
 
-    }
-
-    @Override
-    public CsdlComplexType getComplexType(final FullQualifiedName complexTypeName) {
-        CsdlComplexType complexType = null;
-        if (complexTypeName.equals(CT_ADDRESS_FQN)) {
-            complexType = new CsdlComplexType().setName(CT_ADDRESS_NAME)
-                    .setProperties(Arrays.asList(
-                            new CsdlProperty()
-                                    .setName("City")
-                                    .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName()),
-                            new CsdlProperty()
-                                    .setName("Country")
-                                    .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName())));
-        }
-        return complexType;
     }
 
     @Override
@@ -177,26 +97,13 @@ public class DemoEdmProvider extends CsdlAbstractEdmProvider {
                 entitySet.setType(ET_PRODUCT_FQN);
 
                 // navigation
-                CsdlNavigationPropertyBinding navPropBinding = new CsdlNavigationPropertyBinding();
+               /* CsdlNavigationPropertyBinding navPropBinding = new CsdlNavigationPropertyBinding();
                 navPropBinding.setTarget("Categories"); // the target entity set, where the navigation property points to
                 navPropBinding.setPath("Category"); // the path from entity type to navigation property
                 List<CsdlNavigationPropertyBinding> navPropBindingList = new ArrayList<CsdlNavigationPropertyBinding>();
                 navPropBindingList.add(navPropBinding);
-                entitySet.setNavigationPropertyBindings(navPropBindingList);
+                entitySet.setNavigationPropertyBindings(navPropBindingList);*/
 
-            } else if (entitySetName.equals(ES_CATEGORIES_NAME)) {
-
-                entitySet = new CsdlEntitySet();
-                entitySet.setName(ES_CATEGORIES_NAME);
-                entitySet.setType(ET_CATEGORY_FQN);
-
-                // navigation
-                CsdlNavigationPropertyBinding navPropBinding = new CsdlNavigationPropertyBinding();
-                navPropBinding.setTarget("Products"); // the target entity set, where the navigation property points to
-                navPropBinding.setPath("Products"); // the path from entity type to navigation property
-                List<CsdlNavigationPropertyBinding> navPropBindingList = new ArrayList<CsdlNavigationPropertyBinding>();
-                navPropBindingList.add(navPropBinding);
-                entitySet.setNavigationPropertyBindings(navPropBindingList);
             }
         }
 
@@ -226,13 +133,10 @@ public class DemoEdmProvider extends CsdlAbstractEdmProvider {
         // add EntityTypes
         List<CsdlEntityType> entityTypes = new ArrayList<CsdlEntityType>();
         entityTypes.add(getEntityType(ET_PRODUCT_FQN));
-        entityTypes.add(getEntityType(ET_CATEGORY_FQN));
-        entityTypes.add(getEntityType(ET_SUPPLIER_FQN));
         schema.setEntityTypes(entityTypes);
 
         // add Complex Types
         List<CsdlComplexType> complexTypes = new ArrayList<CsdlComplexType>();
-        complexTypes.add(getComplexType(CT_ADDRESS_FQN));
         schema.setComplexTypes(complexTypes);
 
         // add EntityContainer
@@ -249,14 +153,13 @@ public class DemoEdmProvider extends CsdlAbstractEdmProvider {
     public CsdlEntityContainer getEntityContainer() {
 
         // create EntitySets
-        List<CsdlEntitySet> entitySets = new ArrayList<CsdlEntitySet>();
-        entitySets.add(getEntitySet(CONTAINER, ES_PRODUCTS_NAME));
-        entitySets.add(getEntitySet(CONTAINER, ES_CATEGORIES_NAME));
+        /*List<CsdlEntitySet> entitySets = new ArrayList<CsdlEntitySet>();
+        entitySets.add(getEntitySet(CONTAINER, ES_PRODUCTS_NAME));*/
 
         // create EntityContainer
         CsdlEntityContainer entityContainer = new CsdlEntityContainer();
         entityContainer.setName(CONTAINER_NAME);
-        entityContainer.setEntitySets(entitySets);
+        //entityContainer.setEntitySets(entitySets);
 
         return entityContainer;
     }
