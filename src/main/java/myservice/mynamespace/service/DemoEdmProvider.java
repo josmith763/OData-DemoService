3ae5811f0e769a953623a397ad7333b9507de475
Rob Ratcliffe <rlr@tcliffe.dev>
2021-04-02 11:28:37 -0400

    Add user lastName

--- src/main/java/myservice/mynamespace/service/DemoEdmProvider.java
+++ src/main/java/myservice/mynamespace/service/DemoEdmProvider.java
@@ -152,13 +152,15 @@ public class DemoEdmProvider extends CsdlAbstractEdmProvider {
                     .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
             CsdlProperty firstName = new CsdlProperty().setName("firstName")
                     .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
+            CsdlProperty lastName = new CsdlProperty().setName("lastName")
+                    .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
 
             CsdlPropertyRef propertyRef = new CsdlPropertyRef();
             propertyRef.setName("id");
 
             entityType = new CsdlEntityType();
             entityType.setName(ET_USER_NAME);
-            entityType.setProperties(Arrays.asList(id, username, firstName));
+            entityType.setProperties(Arrays.asList(id, username, firstName, lastName));
             entityType.setKey(Arrays.asList(propertyRef));
         }
 
